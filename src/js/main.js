a219f24741f162efd839fac5fc319fc0aab520af
Martin Hein <martin@herrhein.com>
2022-01-04 15:12:11 +0100

    design changes for followup-pages

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,13 @@
     //remove bootstrap-helper-class from Swift Page Builder
     $('.icon-box-ergo').removeClass('col-sm-2');
 
+    //add helper-class to icon-box-container
+    $('.icon-box-ergo').parent('div').addClass('icon-box-ergo__wrapper')
+
 	/*			parse menu					*/
-	// $('a[href*=#]:not([href=#])').each(function(){
-	// 	$(this).attr('href', $(this)[0].href.replace(location.href.replace(location.hash,''), ''));
-	// });
+	$('a[href*=#]:not([href=#])').each(function(){
+		$(this).attr('href', $(this)[0].href.replace(location.href.replace(location.hash, ''), ''));
+	});
 
-	// $('a[href="'+location.href.replace(location.hash,'')+'"]').on('click', function(){
-	// 	$('html, body').stop().animate({
-	// 		scrollTop: 0
-	// 	}, 600, 'easeInOutExpo');
-
-	// 	history.pushState('', document.title, window.location.pathname + window.location.search);
-	// 	return false;
-	// });
 
 })(jQuery, window);
