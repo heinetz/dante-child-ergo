3a21c4ff39fa9ed816d7be46f36557461bb47f09
Martin Hein <martin@herrhein.com>
2022-01-06 22:29:16 +0100

    add GDPR Google Maps

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,13 @@
     $('.icon-box-ergo').removeClass('col-sm-2');
 
     //add helper-class to icon-box-container
-    $('.icon-box-ergo').parent('div').addClass('icon-box-ergo__wrapper')
+    $('.icon-box-ergo').parent('div').addClass('icon-box-ergo__wrapper');
 
-	/*			parse menu					*/
-	$('a[href*=#]:not([href=#])').each(function(){
-		$(this).attr('href', $(this)[0].href.replace(location.href.replace(location.hash, ''), ''));
-	});
-
+    //add some jq to modify exactly-gdpr-map
+    //$('.exggmap-wrapper iframe').attr('data-src', '');
+    $('.exggmap-caption').html('<p>Hier klicken, um Google Map zu laden.</p><a href="https://policies.google.com/privacy?hl=de" target="_blanc">Google-Datenschutzerklärung</a>');
+    // $('.exggmap-caption a').click(function(e){
+    //     e.stopPropagation();
+    // });
 
 })(jQuery, window);
